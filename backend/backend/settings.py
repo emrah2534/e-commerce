48ea39893f04bb44953a0f08bd15ba12cc46e5f4
emrah2534 <53216415+emrah2534@users.noreply.github.com>
2022-12-18 08:51:47 +0300

    'update'

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ecommerce',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ backend/ecommerce/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework.views import APIView
+
+
+
+urlpatterns = [
+
+]
